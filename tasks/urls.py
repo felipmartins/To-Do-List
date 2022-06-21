9e639a68bf5dde0f5e621eeb771fc9d923963f6d
felipmartins <51343882+felipmartins@users.noreply.github.com>
2022-06-21 09:11:17 -0300

    Add: adiciona caminhos e htmls

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from rest_framework.authtoken.views import obtain_auth_token
 from django.urls import path
+from .views import index
 
 urlpatterns = [
-    
+    path('', index, name='home'),
+    path('login/', auth_views.LoginView.as_view(template_name="login.html"), name='auth-login'),
+    path("logout/", auth_views.LogoutView.as_view(template_name="login.html"), name="auth-logout",),
+    path("api-token-auth/", obtain_auth_token, name="api_token_auth"),
 ]
