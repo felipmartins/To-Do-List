37ceb06a8a7b4b413a42d5a5307fe85f92ce0ed4
felipmartins <51343882+felipmartins@users.noreply.github.com>
2022-06-21 09:10:11 -0300

    Add: Cria tabela de tarefas

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,23 @@
+from contextlib import nullcontext
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Task(models.Model):
+
+    status_list = [
+        (0, "Pendente"),
+        (1, "Em andamento"),
+        (2, "Finalizado")
+    ]
+
+    title = models.CharField(max_length=150, blank=False, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=status_list, default=0)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return (f"""{self.title} -
+                    Criada em {str(self.created_at)[0:19]} - 
+                    Por {self.user.username} - 
+                    Atualizada a última vez em {str(self.updated_at)[0:19]}""")
