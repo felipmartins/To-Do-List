0d68d0a3f1d5fda6151d0e4eccf6e0a5cbc72581
felipmartins <51343882+felipmartins@users.noreply.github.com>
2022-06-21 09:12:19 -0300

    Add: Define a primeira view

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Task
 
-# Create your views here.
+@login_required
+def index(request):
+
+    user_id = request.user.id
+
+    context = {
+        'tasks': Task.objects.all().filter(user=user_id)
+    }
+    return render(request, 'index.html', context)
